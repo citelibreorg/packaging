e589a6b8f28a6c5ff2e72a2b00d2cc48bc7cebe5
Loris Boiteux <lboiteux@yupiik.com>
2025-08-25 16:34:03 +0200

    [docker] package rendezvous with authentication providers

--- citelibre-common/sql/init_db.sh
+++ citelibre-common/sql/init_db.sh
@@ -1,5 +1,11 @@
-echo Creating database $INIT_DATABASE_NAME on host $INIT_DATABASE_HOST for app user $INIT_APP_USER
-
+echo "[INIT DB] Creating database $INIT_DATABASE_NAME on host $INIT_DATABASE_HOST for app user $INIT_APP_USER"
 mariadb --user=root -h $INIT_DATABASE_HOST -e "create database $INIT_DATABASE_NAME"
-mariadb --user=root -h $INIT_DATABASE_HOST -D $INIT_DATABASE_NAME < /init.sql
+
+echo "[INIT DB] process init dump or sql files"
+for filename in /sql/*.sql; do
+  echo "[INIT DB] process file $filename"
+  mariadb --user=root -h $INIT_DATABASE_HOST -D $INIT_DATABASE_NAME < $filename
+done
+
+echo "[INIT DB] Grant all privileges on $INIT_DATABASE_NAME.* TO '$INIT_APP_USER'@'%'"
 mariadb --user=root -h $INIT_DATABASE_HOST -e "GRANT ALL PRIVILEGES ON $INIT_DATABASE_NAME.* TO '$INIT_APP_USER'@'%'"
---
+++ citelibre-rendezvous/docker/sql/02-auth-keycloak.sql
@@ -0,0 +1,5 @@
+INSERT INTO core_datastore VALUES('core.plugins.status.mylutece-database.installed', 'false');
+
+INSERT INTO core_datastore VALUES('core.plugins.status.adminauthenticationoauth2.installed', 'true');
+INSERT INTO core_datastore VALUES('core.plugins.status.mylutece-oauth2.installed','true');
+INSERT INTO core_datastore VALUES('core.plugins.status.oauth2.installed','true');

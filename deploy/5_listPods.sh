a1eb2e65698702146ad22f0d8af45a1532dd137e
Loris Boiteux <lboiteux@yupiik.com>
2025-11-24 15:33:11 +0100

    [deployment] display services URL

--- deploy/5_listPods.sh
+++ deploy/5_listPods.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+echo "----------------------------------------------------"
+echo "- Display pods                                     -"
+echo "----------------------------------------------------"
 minikube kubectl -- get pods -A
+
+echo
+echo "----------------------------------------------------"
+echo "- Display deployed services and their access URL   -"
+echo "----------------------------------------------------"
+for service in $(minikube kubectl -- get services --namespace citelibre -o name | sed 's/service\///');
+  do echo $(minikube service ${service}  --namespace citelibre --url) : ${service};
+done

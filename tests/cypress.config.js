19958d4d712695e47207b478cfad4a1836bbfb9b
Loris Boiteux <lboiteux@yupiik.com>
2025-12-19 10:06:12 +0100

    [#12] fix tutorial URL

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost/citelibre-rendezvous/jsp/admin/AdminMessage.jsp',
     experimentalStudio:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
